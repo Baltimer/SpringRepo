68dda67543768af6ee1524fbb48e48a1942bb5b7
Baltimer <lluiscf92@gmail.com>
2017-05-03 15:00:56 +0200

    Hello World más intento práctica 1

---
+++ Spring-master/pratica_setmana_1.zip_expanded/setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -0,0 +1,23 @@
+package org.formacio.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServeiAlumnat {
+
+	private RepositoriAlumnesMemoria repositori = new RepositoriAlumnesMemoria();
+	/**
+	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
+	 * el corresponent codi.
+	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
+	 * Retorna true si l'alumne s'ha inserit, false si no.
+	 */
+	public boolean matricula (int id, String alumne) {
+		if (alumne == null || alumne == "") {
+			return false;
+		}
+		repositori.altaAlumne(id, alumne);
+		return true;
+	}
+	
+}
